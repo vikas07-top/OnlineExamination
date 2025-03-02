275a661534db49fe714bf7c0a6297cae84dd631b
vikas07-top <kasarlavikas29@gmail.com>
2025-03-02 12:53:16 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,17 @@
+const sharedData = {
+    exams: [
+        { id: 1, name: "Math Exam", date: "2024-12-10" },
+        { id: 2, name: "Science Exam", date: "2024-12-15" },
+    ],
+    users: [
+        {
+            id: 1,
+            name: "User 1",
+            email: "user1@example.com",
+            results: [
+                { examId: 1, score: 85 },
+                { examId: 2, score: 92 },
+            ],
+        },
+    ],
+};
